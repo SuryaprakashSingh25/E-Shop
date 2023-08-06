4aa277c56f3b2b8ea60c5feb94d8f525e2b6aa95
Suraj <singh.suraj1025@gmail.com>
2023-08-06 12:43:20 +0530

    Register Screen Functionality

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -16,6 +16,7 @@ const HomeScreen = () => {
         </Message>
       ) : (
         <>
+          <h1>Latest Products</h1>
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -25,8 +26,6 @@ const HomeScreen = () => {
           </Row>
         </>
       )}
-
-      <h1>Latest Products</h1>
     </>
   );
 };
